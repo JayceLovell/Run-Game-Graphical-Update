02d77b4cd3ecbce149d2d1fe1780ea88626b8d58
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2023-04-04 00:47:46 -0400

    eyes red

--- Assets/Custom Shaders/WindowWithOutline.shader
+++ Assets/Custom Shaders/WindowWithOutline.shader
@@ -1,47 +1,59 @@
 Shader "Custom/WindowWithOutline"
 {
+    // Define editable properties for this shader in the Unity Editor
     Properties
     {
-        _MainTex ("Diffuse", 2D) = "white" {}
-        _OutlineColor ("Outline Color", Color) = (0,0,0,1)
-        _Outline ("Outline Width", Range (.0002,0.1)) = .0005
+        _MainTex ("Diffuse", 2D) = "white" {}        // Window texture
+        _OutlineColor ("Outline Color", Color) = (0,0,0,1)    // Outline color
+        _Outline ("Outline Width", Range (.0002,0.1)) = .0005    // Outline width
     }
+
+    // Define the rendering code for this shader
     SubShader
     {
+        // Set the rendering order for this shader
         Tags { "Queue" = "Geometry-1" }
 
+        // Disable color writes and depth buffer writes for this shader
         ColorMask 0
         ZWrite off
 
+        // Set up the stencil buffer to replace any existing value with 1
         Stencil{
             Ref 1
             Comp always
             Pass replace
         }
 
+        // Define the actual shader code using the CG programming language
         CGPROGRAM
-        #pragma surface surf Lambert vertex:vert
+        #pragma surface surf Lambert vertex:vert    // Set the shader model and vertex function
 
-        sampler2D _MainTex;
-        float _Outline;
-        float4 _OutlineColor;
+        // Define shader variables
+        sampler2D _MainTex;        // Window texture variable
+        float _Outline;        // Outline width variable
+        float4 _OutlineColor;        // Outline color variable
 
+        // Define the input struct that holds the UV coordinates for the texture
         struct Input{
             float2 uv_MainTex;
         };
 
+        // Define the vertex function that offsets the vertex position to create the outline
         void vert (inout appdata_full v){
-        v.vertex.xyz += v.normal * _Outline;
-       }
+            v.vertex.xyz += v.normal * _Outline;
+        }
 
+        // Define the surface function that samples the texture and sets the output colors
         void surf(Input IN, inout SurfaceOutput o){
-            fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
-            o.Albedo = c.rgb;
-            o.Emission = _OutlineColor.rgb;
+            fixed4 c = tex2D(_MainTex, IN.uv_MainTex);    // Sample the texture at the current UV coordinates
+            o.Albedo = c.rgb;    // Set the output Albedo color to the sampled color
+            o.Emission = _OutlineColor.rgb;    // Set the output Emission color to the outline color
         }
         ENDCG
 
-         
     }
+
+    // Define a fallback shader to use if this custom shader fails to compile or run
     FallBack "Diffuse"
 }
--- Assets/_Scripts/Blur.cs
+++ Assets/_Scripts/Blur.cs
@@ -4,46 +4,67 @@ using UnityEngine;
 
 public class Blur : MonoBehaviour
 {
-    [Range(1,64)]
+    // Variables for controlling blur strength and iterations in editor
+    [Range(1, 64)]
     public int integerRange = 1;
     [Range(1, 16)]
     public int iterations = 1;
-    //method which is automatically called by unity after the camera is done rendering
+
+    // Method which is automatically called by Unity after the camera is done rendering
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        // Reduce the resolution of the source texture by integerRange
         int width = source.width / integerRange;
         int height = source.height / integerRange;
+
+        // Create the initial destination texture
         RenderTextureFormat format = source.format;
         RenderTexture[] textures = new RenderTexture[16];
         RenderTexture currentDestination = textures[0] =
-        RenderTexture.GetTemporary(width, height, 0, format);
+            RenderTexture.GetTemporary(width, height, 0, format);
+
+        // Blit the source texture to the initial destination texture
         Graphics.Blit(source, currentDestination);
+
+        // Set the current source texture to the initial destination texture
         RenderTexture currentSource = currentDestination;
-        Graphics.Blit(currentSource, destination);
-        RenderTexture.ReleaseTemporary(currentSource);
+
+        // Loop through the specified number of iterations
         int i = 1;
         for (; i < iterations; i++)
         {
+            // Reduce the width and height by half for each iteration
             width /= 2;
             height /= 2;
+
+            // Create a new destination texture
             currentDestination = textures[i] =
-            RenderTexture.GetTemporary(width, height, 0, format);
+                RenderTexture.GetTemporary(width, height, 0, format);
+
+            // If the height is less than 2, stop iterating
             if (height < 2)
             {
                 break;
             }
-            currentDestination =
-            RenderTexture.GetTemporary(width, height, 0, format);
+
+            // Blit the current source texture to the new destination texture
             Graphics.Blit(currentSource, currentDestination);
+
+            // Release the current source texture
             RenderTexture.ReleaseTemporary(currentSource);
+
+            // Set the current source texture to the new destination texture
             currentSource = currentDestination;
         }
+
+        // Loop through any remaining iterations
         for (; i < iterations; i++)
         {
             Graphics.Blit(currentSource, currentDestination);
-            // RenderTexture.ReleaseTemporary(currentSource);
             currentSource = currentDestination;
         }
+
+        // Loop through the RenderTextures in reverse order and create the final blurred image
         for (i -= 2; i >= 0; i--)
         {
             currentDestination = textures[i];
@@ -52,6 +73,8 @@ public class Blur : MonoBehaviour
             RenderTexture.ReleaseTemporary(currentSource);
             currentSource = currentDestination;
         }
+
+        // Blit the final blurred image to the destination RenderTexture parameter
         Graphics.Blit(currentSource, destination);
     }
 }
